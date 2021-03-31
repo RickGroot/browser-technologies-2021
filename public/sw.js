7ce68a8c2409a09ea61015a05e1ad4ca9fe4f7dd
Rick Groot <rickgroot41@gmail.com>
2021-03-31 09:10:38 +0200

    last update

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 const CORE_CACHE = 1
 const CORE_CACHE_NAME = `core-v${CORE_CACHE}`
-const CORE_ASSETS = ["manifest.json", "/offline"] 
+const CORE_ASSETS = ["manifest.json", "/offline", "css/index.css", "scripts/index-min.js"] 
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
